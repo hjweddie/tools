d95b8c52ce6079641c7f919443e3cf5d887687b8
hjweddie <hjweddie@163.com>
2015-01-13 22:46:03 +0800

    去除多余注释

--- auto_deploy/nginx_conf.py
+++ auto_deploy/nginx_conf.py
@@ -6,12 +6,8 @@ def toggle(path, item_arr, value):
     nc.loadf(path)
 
     parent_id = item_arr[0:-1]
-    print "parent_id:", parent_id
     parent = nc.get(parent_id)
-    print "parent:", parent
     parent_value = nc.get_value(parent)
-    print "parent_value:", parent_value
-    # if isinstance(this, list):
     new_value = []
     for row in parent_value:
         n = row[0]
@@ -25,12 +21,8 @@ def toggle(path, item_arr, value):
                 # to lose effect
                 n = "%s%s" % ('#', n)
         new_row = (n, v)
-        print "new_row:", new_row
         new_value.append(new_row)
-    print "new_value:", new_value
-    print "parent id:", parent_id
     nc.set(parent_id, value=new_value)
-    # elif isinstance(parent, str):
     print "lose effect result:\n", nc.gen_config()
 
 
@@ -38,11 +30,10 @@ path = r'./default'
 take_effect_item_arr = [('upstream', 'http'), '#server']
 lose_effect_item_arr = [('upstream', 'http'), 'server']
 lose_effect_item_arr_test = [('server',), ('location', r'/doc/'), 'alias']
-
 print "--------------------------------------------------"
 toggle(path, take_effect_item_arr, "127.0.0.1:8001")
 print "--------------------------------------------------"
-#toggle(path, lose_effect_item_arr, "127.0.0.1:8002")
+toggle(path, lose_effect_item_arr, "127.0.0.1:8002")
 print "--------------------------------------------------"
 toggle(path, lose_effect_item_arr_test, "/usr/share/doc/")
 print "--------------------------------------------------"
